cb54c36bae667faf05cc31d21faa834facc48cc7
PerezEnric <enriic19@gmail.com>
2020-01-11 11:52:45 +0100

    Song Page almost done

--- assignment03/lib/main.dart
+++ assignment03/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:assignment03/FavsListPage.dart';
 
@@ -41,6 +42,47 @@ class _HomePageState extends State<HomePage> {
             MaterialPageRoute(
               builder: (context)=>FavsListPage(),
             )
+          );
+        }
+      ),
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('Songs').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot){
+          if(!snapshot.hasData){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          List<DocumentSnapshot> docs = snapshot.data.documents;
+          return ListView.separated(
+            itemCount: docs.length,
+            separatorBuilder: (context, index){
+              return Divider(
+                height: 1,
+                indent: 15,
+                endIndent: 15,
+              );
+            },
+            itemBuilder: (context, index){
+              return ListTile(
+                leading: Container(
+                  width: 45,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: Colors.blueGrey,
+                  ),
+                ),
+                title: Text(docs[index].data['name']),
+                subtitle: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(docs[index].data['artist']),
+                    Text(docs[index].data['album']),
+                    Text(docs[index].data['time_min'].toString()+':'+docs[index].data['time_sec'].toString()),
+                  ],
+                ),
+              );
+            }
           );
         }
       ),
